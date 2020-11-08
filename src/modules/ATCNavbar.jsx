c8627b89cae6cb4e7c06605f1033938ba7fbb85f
mmarini <marco.marini@mmarini.org>
2020-11-08 01:33:52 +0100

    issue #65, #67, #68

--- src/modules/ATCNavbar.jsx
+++ src/modules/ATCNavbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Badge, Col, Container, Nav, Navbar } from 'react-bootstrap';
+import { Badge, Col, Container, Nav, Navbar, Row } from 'react-bootstrap';
+import { sprintf } from 'sprintf-js';
 
 function ATCNavBar({ session }) {
 
@@ -11,26 +12,55 @@ function ATCNavBar({ session }) {
       <Navbar.Text></Navbar.Text>
     );
   } else {
-    const { noFlights, noLandedOk, noLandedKo, noExitKo, noExitOk, noCollision } = session
+    const { noFlights, noLandedOk, noLandedKo, noExitKo, noExitOk, noCollision, t } = session
+    const successRate = !!t ? (noLandedOk + noExitOk) * 3600 / t : 0;
+    const wrongRate = !!noFlights ? (noExitKo + noLandedKo) / noFlights * 100 : 0;
+    const collisionRate = !!noFlights ? noCollision / noFlights * 100 : 0;
+
     serviceStatus = (
       <Container fluid>
         <Col>
-          <span><Badge variant="info">Inbound: {noFlights}</Badge></span>
+          <span>
+            <Badge variant="info" pill>Inbound: {sprintf('%d', noFlights)}</Badge>
+          </span>
         </Col >
         <Col>
-          <span><Badge variant="success">Right landed: {noLandedOk}</Badge></span>
+          <span>
+            <Badge variant="success" pill>Success: {sprintf('%.1f fph', successRate)}</Badge>
+          </span>
         </Col >
         <Col>
-          <span><Badge variant="success">Right left: {noExitOk}</Badge></span>
+          <span>
+            <Badge variant="warning" pill>Wrong: {sprintf('%.0f%%', wrongRate)}</Badge>
+          </span>
         </Col >
         <Col>
-          <span><Badge variant="warning">Wrong landed: {noLandedKo}</Badge></span>
+          <span>
+            <Badge variant="danger" pill>Collision: {sprintf('%.0f%%', collisionRate)}</Badge>
+          </span>
         </Col >
         <Col>
-          <span><Badge variant="warning">Wrong left: {noExitKo}</Badge></span>
+          <span>
+            <Badge variant="success">Right landed: {sprintf('%d', noLandedOk)}</Badge>
+          </span>
         </Col >
         <Col>
-          <span><Badge variant="danger">Collisions: {noCollision}</Badge></span>
+          <span>
+            <Badge variant="success">Right left: {sprintf('%d', noExitOk)}</Badge>
+          </span>
+        </Col >
+        <Col>
+          <span>
+            <Badge variant="warning">Wrong landed: {sprintf('%d', noLandedKo)}</Badge>
+          </span>
+        </Col >
+        <Col>
+          <span><Badge variant="warning">Wrong left: {sprintf('%d', noExitKo)}</Badge></span>
+        </Col>
+        <Col>
+          <span>
+            <Badge variant="danger">Collisions: {sprintf('%d', noCollision)}</Badge>
+          </span>
         </Col >
       </Container >
     );
