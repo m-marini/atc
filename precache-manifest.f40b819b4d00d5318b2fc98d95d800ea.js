1dda4f8a44948ff014e87c02b629dbe45ac0de63
m-marini <m-marini@users.noreply.github.com>
2020-12-16 18:46:35 +0000

    Deploying to gh-pages from  @ 168444ae4d85188628754674ef6ba91c70945e07 🚀

---
+++ precache-manifest.f40b819b4d00d5318b2fc98d95d800ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32f28b3359a678079aadf097cba73e0e",
+    "url": "/atc/index.html"
+  },
+  {
+    "revision": "951927c5dbc52ec372bf",
+    "url": "/atc/static/css/2.6b5d40a7.chunk.css"
+  },
+  {
+    "revision": "686d2736adccf0ddda18",
+    "url": "/atc/static/css/main.8dc48b60.chunk.css"
+  },
+  {
+    "revision": "951927c5dbc52ec372bf",
+    "url": "/atc/static/js/2.73f7b801.chunk.js"
+  },
+  {
+    "revision": "c0cc83a1fdad8a0d9a4a95acdbfe4a4b",
+    "url": "/atc/static/js/2.73f7b801.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "686d2736adccf0ddda18",
+    "url": "/atc/static/js/main.c3e05c5d.chunk.js"
+  },
+  {
+    "revision": "8834377b92783b827541",
+    "url": "/atc/static/js/runtime-main.ef74fcec.js"
+  }
+]);
